70912ba763dc4faf48be8ec8358e7b6c6ae43d6b
AbdullahAhmed11 <abdullah77zzx@gmail.com>
2023-02-13 04:16:43 +0200

    addStudentsRepo

--- repo/Repository.cpp
+++ repo/Repository.cpp
@@ -1,15 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include "../model/Model.cpp"
 
 /////////////student
 // interface studentRepository
 class StudentRepository
 {
+public:
+    virtual int addStudent(Student student);
 };
 
 // class studentRepositoryImpl
 class studentRepositoryImpl : public StudentRepository
 {
+private:
+    Student students[25];
+    int index = 0;
+
+public:
+    int addStudent(Student student)
+    {
+        if (index == 25)
+        {
+            cout << "Full Students" << endl;
+        }
+        else
+        {
+            students[index] = student;
+            index++;
+        }
+    }
 };
 /////////////course
 // interface courseRepository

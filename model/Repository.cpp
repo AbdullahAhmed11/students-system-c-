a8be7605bc908774858b6d9089436d6d70f689c3
AbdullahAhmed11 <abdullah77zzx@gmail.com>
2023-02-13 03:50:40 +0200

    implement repoFunction

---
+++ model/Repository.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/////////////student
+// interface studentRepository
+class StudentRepository
+{
+};
+
+// class studentRepositoryImpl
+class studentRepositoryImpl : public StudentRepository
+{
+};
+/////////////course
+// interface courseRepository
+class CourseRepository
+{
+};
+
+// class CourseRepositoryImpl
+class CourseRepositoryImpl : public CourseRepository
+{
+};
+/////////////teacher
+// interface teacherRepository
+class TeacherRepository
+{
+};
+
+// class TeacherRepositoryImpl
+class TeacherRepositoryImpl : public TeacherRepository
+{
+};
